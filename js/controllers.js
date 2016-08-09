7748f9f37a34792f28411bef45826fd632a07332
Zangue <zangue@example.com>
2016-08-09 10:52:14 +0100

    used directive in menu element

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 var controllers = {};
 
 controllers.MenuController = function($scope){
-			$scope.menuElements = [
+			$scope.obj = [
 				{name: 'Downloads', aIcon: 'gn-icon gn-icon-download', url: '#/downloads', subMenu:[{name:'Vector Illustrations', aIcon:'gn-icon gn-icon-illustrator', url:'#'},{name:'Photoshop files', aIcon:'gn-icon gn-icon-photoshop', url:'#'}]},
 				{name: 'Settings', liIcon: '', aIcon: 'gn-icon gn-icon-cog', url: '#/settings', subMenu:[]},
 				{name: 'Help', aIcon: 'gn-icon gn-icon-help', url: '#/help', subMenu:[]},
@@ -30,5 +30,37 @@ controllers.ArchivesController = function($scope){
 };
 
 
-
 nexusModule.controller(controllers);
+
+
+nexusModule.directive('passObject', function() {
+    return {
+        restrict: 'E',
+        scope: { obj: '=' },
+        controller:'MenuController',
+        template: '<ul id="gn-menu" class="gn-menu-main">\
+        <li class="gn-trigger"><a class="gn-icon gn-icon-menu"><span>Menu</span></a><nav class="gn-menu-wrapper">\
+        <div class="gn-scroller">\
+              <ul class="gn-menu"  >\
+                <li class="gn-search-item">\
+                  <input placeholder="Search" type="search" class="gn-search">\
+                  <a class="gn-icon gn-icon-search"><span>Search</span></a>\
+                </li>\
+                  <li ng-repeat="element in obj">\
+                    <a class="{{element.aIcon}}" href="{{element.url}}">{{element.name}}</a>\
+                    <ul class="gn-submenu">\
+                      <li ng-repeat="sub in element.subMenu">\
+                        <a class="{{sub.aIcon}}" href="{{sub.url}}">{{sub.name}}</a>\
+                      </li>\
+                    </ul>\
+                  </li>\
+              </ul>\
+            </div><!-- /gn-scroller -->\
+          </nav>\
+        </li>\
+        <li><a href="http://tympanus.net/codrops">Codrops</a></li>\
+        <li><a class="codrops-icon codrops-icon-prev" href="http://tympanus.net/Development/HeaderEffects/"><span>Previous Demo</span></a></li>\
+        <li><a class="codrops-icon codrops-icon-drop" href="http://tympanus.net/codrops/?p=16030"><span>Back to the Codrops Article</span></a></li>\
+</ul>'
+    };
+});
